8d7f11ae67903c04af0431dd22906c8ee9a15967
SmallJoker <st.rentsch@hotmail.com>
2014-07-23 12:47:12 +0200

    Some updates Elminate 2d-skins Skinlist wrap around Save last visited page Cleaner codes -- Full compatible to pthe previous version

--- u_skins/players.lua
+++ u_skins/players.lua
@@ -1,16 +1,14 @@
-u_skins.file = minetest.get_worldpath() .. "/u_skins.mt"
-
-u_skins.load = function()
+u_skins.load_players = function()
 	local file = io.open(u_skins.file, "r")
 	if file then
 		for line in file:lines() do
-			local data = string.split(line, ' ', 2)
+			local data = string.split(line, " ", 2)
 			u_skins.u_skins[data[1]] = data[2]
 		end
 		io.close(file)
 	end
 end
-u_skins.load()
+u_skins.load_players()
 
 local ttime = 0
 minetest.register_globalstep(function(t)
@@ -18,25 +16,22 @@ minetest.register_globalstep(function(t)
 	if ttime < 360 then --every 6min'
 		return
 	end
-	if(u_skins.file_save) then
-		u_skins.file_save = false
-		u_skins.save()
-	end
 	ttime = 0
+	u_skins.save()
 end)
 
-minetest.register_on_shutdown(function()
-	if(u_skins.file_save) then
-		u_skins.save()
-	end
-end)
+minetest.register_on_shutdown(function() u_skins.save() end)
 
 u_skins.save = function()
-	local output = io.open(u_skins.file,'w')
+	if not u_skins.file_save then
+		return
+	end
+	u_skins.file_save = false
+	local output = io.open(u_skins.file, "w")
 	for name, skin in pairs(u_skins.u_skins) do
 		if name and skin then
 			if skin ~= "character_1" then
-				output:write(name .. " " .. skin .. "\n")
+				output:write(name.." "..skin.."\n")
 			end
 		end
 	end
--- u_skins/skinlist.lua
+++ u_skins/skinlist.lua
@@ -8,15 +8,15 @@ while fetched_skip < 40 do
 	local name = "character_"..id
 	local file = io.open(u_skins.modpath.."/meta/"..name..".txt", "r")
 	if file then
-		local data = string.split(file:read('*all'), "\n", 3)
+		local data = string.split(file:read("*all"), "\n", 3)
 		file:close()
 		
 		u_skins.list[internal_id] = name
 		u_skins.meta[name] = {}
 		u_skins.meta[name].name = data[1]
 		u_skins.meta[name].author = data[2]
-		u_skins.meta[name].description = "" --not done
-		u_skins.meta[name].comment = data[3]
+		u_skins.meta[name].license = data[3]
+		u_skins.meta[name].description = "" --what's that??
 		
 		fetched_skip = 0
 		internal_id = internal_id + 1
